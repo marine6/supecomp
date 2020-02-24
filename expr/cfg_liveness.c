77370e9202261c64c016a3b35eb0988a5e92eb72
MarineMB <marine.dumesnil@hotmail.fr>
2020-02-24 16:52:13 +0100

    Questions 7.1 and 7.2

--- expr/cfg_liveness.c
+++ expr/cfg_liveness.c
@@ -16,12 +16,41 @@ void print_mapping(FILE* f, list* map){
 }
 
 list* expr_used_vars(struct expression* e){
- return NULL;
+    switch (e->etype) {
+        case EINT:
+            return NULL;
+        case EVAR:
+            return cons(e->var.s, NULL);
+        case EUNOP:
+            return expr_used_vars(e->unop.e)
+        case EBINOP:
+            struct list *l = expr_used_vars(e->binop.e1);
+            if (l == NULL) {
+                return expr_used_vars(e->binop.e2);
+            }
+            struct list *l2 = l;
+            while (l2->next != NULL) {
+                l2 = l2->next;
+            }
+            l2->next = expr_used_vars(e->binop.e2);
+            return l;
+    }
+    return NULL;
 }
 
 
 list* live_after(node_t* n, list* map){
   list* live_aft = NULL;
+  list *l = live_aft;
+  list *succs = succs_node(n);
+  while (succs != NULL) {
+    list *vars = assoc_get(map, succs->elt);
+    while (vars != NULL) {
+        l->elt = vars->elt;
+        l = l->next;
+        vars = vars->next;
+    }
+  }
   return live_aft;
 }
 
