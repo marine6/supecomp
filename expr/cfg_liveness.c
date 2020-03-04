0c94abb8a737baa0cb109eb9c72f0e7db4d6facc
jusitn <fontjustin@gmail.com>
2020-03-04 09:55:07 +0100

    tp 4 works

--- expr/cfg_liveness.c
+++ expr/cfg_liveness.c
@@ -24,33 +24,31 @@ list* expr_used_vars(struct expression* e){
         case EUNOP:
             return expr_used_vars(e->unop.e);
         case EBINOP:
-            if (expr_used_vars(e->binop.e1) == NULL) {
-                return expr_used_vars(e->binop.e2);
-            }
-            struct list *l2 = expr_used_vars(e->binop.e1);
-            while (l2->next != NULL) {
-                l2 = l2->next;
-            }
-            l2->next = expr_used_vars(e->binop.e2);
-            return expr_used_vars(e->binop.e1);
+            return  concat(expr_used_vars(e->binop.e1),expr_used_vars(e->binop.e2));
     }
     return NULL;
 }
 
 
 list* live_after(node_t* n, list* map){
-  list* live_aft = NULL;
-  list *l = live_aft;
-  list *succs = succs_node(n);
-  while (succs != NULL) {
-    list *vars = assoc_get(map, succs->elt);
-    while (vars != NULL) {
-        l->elt = vars->elt;
-        l = l->next;
-        vars = vars->next;
+    list *live_aft = NULL;
+    list *succesors = succs_node(n);
+    list *temp_list = map;
+    while (temp_list) {
+        pair *temp_pair = temp_list->elt;
+        if (list_in_int(succesors, (int)(unsigned long)temp_pair->fst)) {
+            list *temp_temp_list = temp_pair->snd;
+            while (temp_temp_list) {
+                if (!list_in_string(live_aft, temp_temp_list->elt)) {
+                    live_aft =
+                            list_append_string(live_aft, temp_temp_list->elt);
+                }
+                temp_temp_list = temp_temp_list->next;
+            }
+        }
+        temp_list = temp_list->next;
     }
-  }
-  return live_aft;
+    return live_aft;
 }
 
 list* live_before(list* live_aft, node_t* n){
@@ -78,7 +76,6 @@ list* live_before(list* live_aft, node_t* n){
 
     }
     list *temp_list = live_aft;
-    // Adding the elements in live_before excluding the dead ones
     while (temp_list) {
         if ((!list_in_string(live_before, temp_list->elt)) &&
             (temp_list->elt != to_exclude)) {
@@ -91,37 +88,42 @@ list* live_before(list* live_aft, node_t* n){
 
 int new_changes;
 
-list* liveness_graph(list* map, cfg* c){
-    // Keep bool to see if there are new variables alive
-    int new_changes = 0;
-    list *l_map = map;
+list *liveness_graph(list *map, cfg *c) {
+    new_changes = 0;
+    list *new_map = map;
 
     // Parsing nodes in ascending order
-    cfg *l_c = c;
-    while (l_c) {
-        list **temp_assoc_get = assoc_get(map, (void *) (unsigned long) l_c->id);
-        list *l_live_before = NULL;
+    cfg *temp_c = c;
+    while (temp_c) {
+        // List of alive variables before temp_c->id in_k
+        list **temp_assoc_get = assoc_get(map, (void *) (unsigned long) temp_c->id);
+        list *live_bef = NULL;
 
         if (temp_assoc_get != NULL) {
-            l_live_before = *temp_assoc_get;
+            live_bef = *temp_assoc_get;
         }
 
-        list *live_bef2 = live_before(live_after(l_c->node, map), l_c->node);
+        // Next iteration of alive variables
+        list *live_bef2 =
+                live_before(live_after(temp_c->node, map), temp_c->node);
 
-        new_changes = new_changes || (1 - list_string_incl(live_bef2, l_live_before));
+        // Changes ?
+        new_changes = new_changes || (1 - list_string_incl(live_bef2, live_bef));
 
-        l_map = assoc_set(l_map, (void *) (unsigned long) l_c->id, live_bef2);
-        l_c = l_c->next;
+        // Update map
+        new_map = assoc_set(new_map, (void *) (unsigned long) temp_c->id, live_bef2);
+        temp_c = temp_c->next;
     }
-    return l_map;
+
+    return new_map;
 }
 
-list* liveness_prog(cfg_prog* p){
+list *liveness_prog(cfg_prog *p) {
     list *map = NULL;
     new_changes = 1;
     while (new_changes) {
         map = liveness_graph(map, p->graph);
-        print_mapping(stdout,map);
+        // print_mapping(stdout,map);
     }
     return map;
 }
